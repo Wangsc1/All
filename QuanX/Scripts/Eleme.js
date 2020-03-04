8394ddf8b4b3dc030b9fe0aa66b834b57483cb69
Wangsc1 <Wangsc1M@gmail.com>
2020-03-04 17:14:30 +0800

    Eleme.js

--- QuanX/Scripts/Eleme.js
+++ QuanX/Scripts/Eleme.js
@@ -1,28 +1,40 @@
-
 /*
 参考chavyleung和NobyDa的写法
+
 //饿了么
+
 > 代码已同时兼容 Surge & QuanX, 使用同一份签到脚本即可
+
+
 ## 配置 (Surge)
+
 ```properties
 [MITM]
 h5.ele.me
+
 [Script]
-http-request ^https:\/\/h5\.ele\.me\/restapi\/eus\/v\d\/current_user$ script-path=https://raw.githubusercontent.com/Wangsc1/QuantumultX/master/Scripts/Eleme_Cookie.js
-cron "0 12 22 * * *" script-path=https://raw.githubusercontent.com/Wangsc1/QuantumultX/master/Scripts/Eleme.js
-
-
+http-request ^https:\/\/h5\.ele\.me\/restapi\/eus\/v\d\/current_user$ script-path=https://raw.githubusercontent.com/nzw9314/QuantumultX/master/Script/elemGetCookies.js
+cron "0 5 0 * * *" script-path=https://raw.githubusercontent.com/nzw9314/QuantumultX/master/Script/elemSign.js
 ```
+
 ## 配置 (QuanX)
+
 ```properties
 [MITM]
 h5.ele.me
+
 [rewrite_local]
-^https:\/\/h5\.ele\.me\/restapi\/eus\/v\d\/current_user$ url script-request-header Quanx/Scripts/Eleme_Cookie.js
+
+^https:\/\/h5\.ele\.me\/restapi\/eus\/v\d\/current_user$ url script-request-header nzw9314/Script/elemGetCookies.js
+
+
+
 [task_local]
 5 0 * * * elemSign.js
 ```
+
 ## 说明
+
 1. 先把h5.ele.me`加到`[MITM]`
 2. 再配置重写规则:
    - Surge: 把两条远程脚本放到`[Script]`
@@ -30,57 +42,73 @@ h5.ele.me
 3. 打开 APP, 访问下`我的`>`签到领红包`(左上角)
 4. 系统提示: `获取Cookie: 成功` （如果不提示获取成功, 尝试杀进程再进`个人`>`每日签到`）
 5. 最后就可以把第 1 条脚本注释掉了
+
 > 第 1 条脚本是用来获取 cookie 的, 用浏览器访问一次获取 cookie 成功后就可以删掉或注释掉了, 但请确保在`登录成功`后再获取 cookie.
+
 > 第 2 条脚本是签到脚本, 每天`00:05:00`执行一次.
 */
 
 
 const cookieName = '饿了么'
 const cookieKey = 'cookie_elem'
-const UserId='user_id_elem'
+const UserId = 'user_id_elem'
 const sy = init()
-var cookieVal =sy.getdata(cookieKey);
-var regx=/USERID=\d+/;
+var cookieVal = sy.getdata(cookieKey);
+var regx = /USERID=\d+/;
 
-var userid=cookieVal.match(regx)[0];
-userid=userid.replace('USERID=','');
+var userid = cookieVal.match(regx)[0];
+userid = userid.replace('USERID=', '');
 
 
-var headerscommon={
-  'Content-Type':'application/json',
-  'Cookie':cookieVal,
-  'f-refer':'wv_h5',
-  'Origin':'https://tb.ele.me',
-   'Referer':'https://tb.ele.me/wow/zele/act/qiandao?wh_biz=tm&source=main',
-   'User-Agent':'Rajax/1 Apple/iPhone11,8 iOS/13.3 Eleme/8.29.6 ID/BFA5A018-7070-4341-9DEF-763E3B23EA282; IsJailbroken/1 Mozilla/5.0 (iPhone; CPU iPhone OS 13_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 AliApp(ELMC/8.29.6) UT4Aplus/0.0.4 WindVane/8.6.0 828x1792 WK'
+var headerscommon = {
+  'Content-Type': 'application/json',
+  'Cookie': cookieVal,
+  'f-refer': 'wv_h5',
+  'Origin': 'https://tb.ele.me',
+  'Referer': 'https://tb.ele.me/wow/zele/act/qiandao?wh_biz=tm&source=main',
+  'User-Agent': 'Rajax/1 Apple/iPhone11,8 iOS/13.3 Eleme/8.29.6 ID/BFA5A018-7070-4341-9DEF-763E3B23EA282; IsJailbroken/1 Mozilla/5.0 (iPhone; CPU iPhone OS 13_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 AliApp(ELMC/8.29.6) UT4Aplus/0.0.4 WindVane/8.6.0 828x1792 WK'
 }
 
 //签到结果
-var signresult='';
+var signresult = '';
 
-//翻牌JSON
-var turnstr='';
 //翻牌结果
-var turnresult=new Array;
+var turnstr = '翻牌结果: ';
+//翻牌奖励
+var turnresult = new Array;
 
-
+//签到奖励
+var sign_result = new Array;
 
 var hisresult;
 sign()
 
 function sign() {
-  let data = dosign().then( (data) => {                                         
-         
-          doturnover().then( (data) => {   
 
-            dosignhis().then( (data) => {   
+  dosignhis().then((data) => {
+    if (hisresult) {
+     if (hisresult.has_signed_in_today) {
+        signresult = `签到结果: 重复❗ 已连续签到${hisresult.current_day+1}天`;
+        turnstr=turnstr+'无';
+        doNotify();
+        sy.done()
+      }
+      else {
+        dosign().then((data) => {
+            doturnover(1,200).then((data) => {
+              doshare().then((data) => {
 
-              doNotify();
-              sy.done()
-            })
+                doturnover(2,3000).then((data) => {
+      
+                  doNotify();
+                  sy.done()
+                })
+              })
           })
-        
-      });
+        })
+      }
+    }
+  });
 }
 
 function dosign() {
@@ -88,50 +116,50 @@ function dosign() {
   return new Promise(resolve => {
     setTimeout(() => {
 
-      try{
-        var endurl='/sign_in'
+      try {
+        var endurl = '/sign_in'
         url = { url: `https://h5.ele.me/restapi/member/v2/users/`, headers: headerscommon }
         if (cookieVal == undefined || cookieVal == "0" || cookieVal == null) {
           sy.msg(cookieName, "未获取Cookie", '');
           return;
         }
-        
+
         url.url += userid;
         url.url += endurl;
-        sy.log(url.url);
+
         sy.post(url, (error, response, data) => {
-          sy.log(response.status);
+          var obj = JSON.parse(data);
           if (response.status == 200) {
-            signresult = '签到结果: 成功🎉'
-            
-           
+            signresult = `签到结果: 成功🎉 已连续签到${hisresult.current_day+2}天`
+            sign_result = obj;
+
           } else if (response.status == 400) {
-            signresult = '签到结果: 重复❗'
-          
+            signresult = `签到结果: 重复❗ 已连续签到${hisresult.current_day}天`
+
           }
           else {
-            signresult = '签到结果: 未知❗'
+            signresult = `签到结果: 未知❗ 已连续签到${hisresult.current_day}天`
           }
           resolve('done');
         })
       }
-      catch(erre){
+      catch (erre) {
         resolve('done')
       }
     })
   })
 }
 
-function doturnover() {
+function doturnover(count,time) {
 
   return new Promise(resolve => {
     setTimeout(() => {
 
-      try{
-        var endurl='/sign_in/daily/prize'
-        let body={"channel": "app","index": 0,"longitude": 116.334716796875,"latitude": 59.73897171020508};
-        url = { 
-          url: `https://h5.ele.me/restapi/member/v2/users/`, 
+      try {
+        var endurl = '/sign_in/daily/prize'
+        let body = { "channel": "app", "index": random(0, 3), "longitude": 116.334716796875, "latitude": 59.73897171020508 };
+        url = {
+          url: `https://h5.ele.me/restapi/member/v2/users/`,
           headers: headerscommon,
           body: JSON.stringify(body)
         }
@@ -141,29 +169,67 @@ function doturnover() {
         }
         url.url += userid;
         url.url += endurl;
-        //headers['Content-Type']='application/json';
-        sy.log(url);
         sy.post(url, (error, response, data) => {
-         console.log(response);
-          var obj=JSON.parse(data);
-
+          var obj = JSON.parse(data);
+          sy.log(count);
           if (response.status == 200) {
-            turnstr = '翻牌结果: 成功🎉'
-            turnresult=obj;
-           
+            turnstr = turnstr + `成功(${count})🎉 `
+            for (var i in obj) {
+              turnresult.push(obj[i]);
+            }
+
           } else if (response.status == 400) {
-            turnstr = '翻牌结果: 重复❗'
-          
+            turnstr = turnstr + `重复(${count})❗ `
+
           }
           else {
-            turnstr = '翻牌结果: 未知❗'
+            turnstr = turnstr + `未知(${count})❗ `
           }
 
 
           resolve('done');
         })
       }
-      catch(erre){
+      catch (erre) {
+        resolve('done')
+      }
+    },time)
+  })
+}
+
+function doshare() {
+
+  return new Promise(resolve => {
+    setTimeout(() => {
+
+      try {
+        var endurl = '/sign_in/wechat'
+        let body = { "channel": "app" };
+        url = {
+          url: `https://h5.ele.me/restapi/member/v1/users/`,
+          headers: headerscommon,
+          body: JSON.stringify(body)
+        }
+        if (cookieVal == undefined || cookieVal == "0" || cookieVal == null) {
+          sy.msg(cookieName, "未获取Cookie", '');
+          return;
+        }
+        url.url += userid;
+        url.url += endurl;
+        sy.post(url, (error, response, data) => {
+          if (response.status == 200) {
+
+            sy.log("分享微信成功");
+          }
+          else {
+            sy.log("分享微信失败");
+          }
+
+
+          resolve('done');
+        })
+      }
+      catch (erre) {
         resolve('done')
       }
     })
@@ -175,8 +241,8 @@ function dosignhis() {
   return new Promise(resolve => {
     setTimeout(() => {
 
-      try{
-        var endurl='/sign_in/info'
+      try {
+        var endurl = '/sign_in/info'
         url = { url: `https://h5.ele.me/restapi/member/v1/users/`, headers: headerscommon }
         if (cookieVal == undefined || cookieVal == "0" || cookieVal == null) {
           sy.msg(cookieName, "未获取Cookie", '');
@@ -184,55 +250,39 @@ function dosignhis() {
         }
         url.url += userid;
         url.url += endurl;
-        sy.log(url.url);
         sy.get(url, (error, response, data) => {
-         
-          var obj=JSON.parse(data);
 
-          hisresult=obj;
+          var obj = JSON.parse(data);
+
+          hisresult = obj;
 
 
           resolve('done');
         })
       }
-      catch(erre){
+      catch (erre) {
         resolve('done')
       }
     })
   })
 }
 
-function doNotify(){
-
-    console.log(hisresult);
-    console.log(turnresult);
-    console.log(turnstr);
-    var ret=signresult;
-    var signday=0;
-    for(var i=0;i<hisresult.statuses.length;i++){
-        if(hisresult.statuses[i]==1){
-          signday++;
-        }
+function doNotify() {
+  console.log(turnresult);
+  var ret = signresult+'\n';
+  for (var i = 0; i < sign_result.length; i++) {
+    ret = ret + '***获得：' + sign_result[i].name + '(' + sign_result[i].amount + ')元🧧\n';
+  }
+  ret = ret + turnstr + '\n';
+  for (var i = 0; i < turnresult.length; i++) {
+    if (turnresult[i].status == 1) {
+      ret = ret + '***获得：' + turnresult[i].prizes[0].name + '(' + turnresult[i].prizes[0].amount + ')元🧧\n';
     }
-    ret=ret+',已连续签到'+signday+'天\n';
-    ret=ret+turnstr;
-    for(var i=0;i<turnresult.length;i++){
-        if(turnresult[i].status==1){
-            ret=ret+' 获得：'+turnresult[i].prizes[0].name+'('+turnresult[i].prizes[0].amount+')元🧧';
-        }
-    }
-    ret=ret+'\n';
-    ret=ret+'签到3天得3元红包，10天抽10-200元🧧';
+  }
+  ret = ret + '签到3天得3元红包，7天抽10-200元🧧';
 
-    sy.msg('饿了么','',ret);
+  sy.msg('饿了么', '', ret);
 }
-
-
-
-
-
-
-
 
 
 
@@ -267,15 +317,15 @@ function init() {
   }
   post = (options, callback) => {
     if (isQuanX()) {
-        if (typeof options == "string") options = { url: options }
-        options["method"] = "POST"
-        $task.fetch(options).then(response => {
-            response["status"] = response.statusCode
-            callback(null, response, response.body)
-        }, reason => callback(reason.error, null, null))
+      if (typeof options == "string") options = { url: options }
+      options["method"] = "POST"
+      $task.fetch(options).then(response => {
+        response["status"] = response.statusCode
+        callback(null, response, response.body)
+      }, reason => callback(reason.error, null, null))
     }
     if (isSurge()) $httpClient.post(options, callback)
-}
+  }
   done = (value = {}) => {
     $done(value)
   }
