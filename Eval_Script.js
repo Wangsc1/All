76b7498f4a8ec11236402aca806d2a52b7d57a36
Wangsc1 <Wangsc1M@gmail.com>
2020-03-10 19:04:59 +0800

    Eval_Script.js

--- Eval_Script.js
+++ Eval_Script.js
@@ -38,8 +38,7 @@ https://raw.githubusercontent.com/Wangsc1/All/master/Js_Update.conf
 [eval_local]
 // custom local...
 // 淘宝
-response ^https?://(trade-acs|amdc)\.m\.taobao\.com/(gw/mtop\.taobao\.detail\.getdetail|amdc/mobileDispatch) eval https://raw.githubusercontent.com/yichahucha/surge/master/tb_price.js
-hostname = amdc.m.taobao.com,trade-acs.m.taobao.com
+^https?://(trade-acs|amdc)\.m\.taobao\.com/(gw/mtop\.taobao\.detail\.getdetail|amdc/mobileDispatch) url script-response-body https://raw.githubusercontent.com/yichahucha/surge/master/tb_price.js
 
 `
 
