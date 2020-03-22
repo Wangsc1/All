45d959ff167017719a14d080b8ea9034122dc23e
Wangsc1 <Wangsc1M@gmail.com>
2020-03-22 14:32:41 +0800

    AppMonitor_Add

--- QuanX/Scripts/AppMonitor.js
+++ QuanX/Scripts/AppMonitor.js
@@ -1,108 +1,58 @@
-console.log("AppMonitor：运行");
-let apps=
-["1444671526","1436650069","1314212521","1347998487","1443988620","1449412357","1164801111","1495946973","333710667","961390574","373311252","673907758","1423330822","945993620","393670998","1154746981","390017969","1312014438","989565871","440488550","1134218562","1373567447","1261944766","1049254261","1067198688","1371929193","1489780246","697927927","718043190","360593530","284666222","1490527415","1455832781","469338840","1355476695"];
-let reg="us";
-let notifys=[];
-format_apps(apps);
-function format_apps(x) {
-    let apps_f={};
-    x.forEach((n)=>{
-        if(/^[a-zA-Z0-9:/|\-_\s]{1,}$/.test(n))
-        {
-            n=n.replace(/[/|\-_\s]/g,":");
-            let n_n=n.split(":");
-            if(n_n.length===1){
-                if(apps_f.hasOwnProperty(reg)){
-                    apps_f[reg].push(n_n);
-                }
-                else
-                {
-                    apps_f[reg]=[];
-                    apps_f[reg].push(n_n[0])
-                }
-            }
-            else if(n_n.length===2){
-                if(apps_f.hasOwnProperty(n_n[1])){
-                    apps_f[n_n[1]].push(n_n[0]);
-                }
-                else
-                {
-                    apps_f[n_n[1]]=[];
-                    apps_f[n_n[1]].push(n_n[0])
-                }
-            }
-            else{
-                notifys.push(`ID格式错误:【${n}】`)
-            }
-        }
-        else{
-            notifys.push(`ID格式错误:【${n}】`)
-        }
-    });
-    if(Object.keys(apps_f).length>0){
-        post_data(apps_f);
-    }
+console.log("APP监控运行")
+let
+apps=["1477376905","1444671526","1436650069","1314212521","1347998487","1443988620","1449412357","1164801111","1495946973","333710667","961390574","373311252","673907758","1423330822","945993620","393670998","1154746981","390017969","1312014438","989565871","440488550","1134218562","1373567447","1261944766","1049254261","1067198688","1371929193","1489780246","697927927","718043190","360593530","284666222","1490527415","1455832781","469338840","1355476695"]
+
+let reg="us"
+let config={
+  url:'https://itunes.apple.com/lookup?id=' + apps + "&country=" + reg,
+  method:"post"
 }
-async function post_data(d) {
-    try{
-        let app_monitor=$prefs.valueForKey("app_monitor");
-        if(app_monitor===""||app_monitor===undefined){
-            app_monitor={}
+$task.fetch(config).then((res)=>{
+  let results=JSON.parse(res.body).results
+  if(results.length>0){
+    let app_monitor=$prefs.valueForKey("app_monitor");
+    if(app_monitor==""||app_monitor==undefined){
+      app_monitor={}
+    }
+    else{
+      app_monitor=JSON.parse(app_monitor)
+    }
+    let notifys=[]
+    let infos={}
+    results.forEach((x=>{
+      infos[x.trackId]={
+        n:x.trackName,
+        v:x.version,
+        p:x.formattedPrice
+      }
+      if(app_monitor.hasOwnProperty(x.trackId)){
+      if(JSON.stringify(app_monitor[x.trackId])!=JSON.stringify(infos[x.trackId])){
+        if(x.version!=app_monitor[x.trackId].version){
+          notifys.push(`👉 ${x.trackName}：
+🏷 版本升级：${x.version}`)
         }
-        else{
-            app_monitor=JSON.parse(app_monitor)
-        }
-        let infos={};
-        await Promise.all(Object.keys(d).map(async (k)=>{
-            let config={
-                url:'https://itunes.apple.com/lookup?id=' + d[k] + "&country=" + k,
-                method:"post"
-            };
-            await $task.fetch(config).then((res)=>{
-                let results=JSON.parse(res.body).results;
-                if(Array.isArray(results)&&results.length>0){
-                    results.forEach((x=>{
-                        infos[x.trackId]={
-                            n:x.trackName,
-                            v:x.version,
-                            p:x.formattedPrice
-                        };
-                        if(app_monitor.hasOwnProperty(x.trackId)){
-                            if(JSON.stringify(app_monitor[x.trackId])!==JSON.stringify(infos[x.trackId])){
-                                if(x.version!==app_monitor[x.trackId].v){
-                                    notifys.push(`📲 ${x.trackName}:
-🏷 版本：${app_monitor[x.trackId].v} 👉 ${x.version}`)
-                                }
-                                if(x.formattedPrice!==app_monitor[x.trackId].p){
-                                    notifys.push(`📲 ${x.trackName}:
-〽️ 价格：${app_monitor[x.trackId].p} 👉 ${x.formattedPrice}`)
-                                }
-                            }}
-                        else{
-                            notifys.push(`📲 ${x.trackName}:
+        if(x.formattedPrice!=app_monitor[x.trackId].formattedPrice){
+          notifys.push(`👉 ${x.trackName}：
+〽️ 价格变化：${x.formattedPrice}`)
+                }
+      }}
+      else{
+        notifys.push(`👉 ${x.trackName}：
 🏷 版本：${x.version}  /  〽️ 价格：${x.formattedPrice}`)
-                        }
-                    }));
-                }
-                return Promise.resolve()
-            }).catch((e)=>{
-                console.log(e);
-            });
-        }));
-        infos=JSON.stringify(infos);
-        $prefs.setValueForKey(infos,"app_monitor");
-        if(notifys.length>0){
-            notify(notifys)
-        }
-        else{
-            console.log("AppMonitor：无变化")
-        }
-    }catch (e) {
-        console.log(e);
+      }
+    }))
+    infos=JSON.stringify(infos)
+    $prefs.setValueForKey(infos,"app_monitor")
+    if(notifys.length>0){
+      notify(notifys)
     }
-}
+    else{
+      console.log("APP监控：版本及价格无变化")
+    }
+  }
+})
 function notify(notifys){
-    notifys=notifys.join("\n");
-    console.log(notifys);
-    $notify("AppMonitor","",notifys)
+  notifys=notifys.join("\n")
+  console.log(notifys)
+  $notify("AppMonitor","",notifys)
 }
