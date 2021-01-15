e0d6889a9c3808dc8cb92c626a477d82eec3b76c
Wangsc1 <Wangsc1M@gmail.com>
2021-01-15 16:42:53 +0800

    Emby.js

--- Scripts/Emby.js
+++ Scripts/Emby.js
@@ -1,7 +1,7 @@
 if
 ($request.url.indexOf('mb3admin.com/admin/service/registration/validateDevice') != -1) {
     if($response.status!=200){
-        $notification.post("EmbyPremiere 已激活", "", "");
+        //$notification.post("EmbyPremiere 已激活", "", "");
         $done({status: 200, headers: $response.headers, body: '{"cacheExpirationDays":999,"resultCode":"GOOD","message":"Device Valid"}' })
     }else{
         $done({})
