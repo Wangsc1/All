440e9414d1271f497d5ea7f854f2799c6572279d
Wangsc1 <Wangsc1M@gmail.com>
2021-04-20 10:10:19 +0800

    sub_info.js

--- Scripts/sub_info.js
+++ Scripts/sub_info.js
@@ -4,10 +4,10 @@
 Surge显示机场的流量信息，url请求头中必须带有流量信息，并且需要urlencode
 
 [Proxy Group]
-DlerCloud = select, policy-path=http://t.tt?url=, update-interval=3600
+ExFlux = select, policy-path=http://t.tt?url=, update-interval=3600
 
 [Script]
-Sub_info = type=http-request,pattern=https://raw.githubusercontent.com/Wangsc1/All/master/Scripts/Sub_info.js
+sub_info = type=http-request,pattern=https://raw.githubusercontent.com/Wangsc1/All/master/Scripts/Sub_info.js
 */
 
 let params = getUrlParams($request.url);
