820e35f0299a3582793622d91ac48088106172b5
Wangsc1 <Wangsc1M@gmail.com>
2021-09-27 21:24:14 +0800

    modified

--- Scripts/NF_Check.js
+++ Scripts/NF_Check.js
@@ -17,7 +17,7 @@ const AREA_TEST_FILM_ID = 80018499
       }
 
       result['style'] = 'good'
-      result['content'] = '🎉 完整解锁 Netflix，解锁区域：' + code.toUpperCase()
+      result['content'] = '🎉 完整解锁Netflix,解锁区域:' + code.toUpperCase()
       return Promise.reject('BreakSignal')
     })
     .then((code) => {
@@ -26,13 +26,13 @@ const AREA_TEST_FILM_ID = 80018499
       }
 
       result['style'] = 'info'
-      result['content'] = '🥲 仅解锁自制剧，解锁区域：' + code.toUpperCase()
+      result['content'] = '🥲 仅解锁自制剧,解锁区域:' + code.toUpperCase()
       return Promise.reject('BreakSignal')
     })
     .catch((error) => {
       if (error === 'Not Available') {
         result['style'] = 'alert'
-        result['content'] = '😭 不支持解锁 Netflix'
+        result['content'] = '😭 不支持解锁Netflix'
         return
       }
     })
