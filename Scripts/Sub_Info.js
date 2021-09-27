f395926b78d61b1443a452dee83e89b584b77f20
Wangsc1 <Wangsc1M@gmail.com>
2021-09-27 14:40:01 +0800

    Sub_Info.js

--- Scripts/Sub_Info.js
+++ Scripts/Sub_Info.js
@@ -1,61 +1,127 @@
 /*
-作者 @mieqq
+Surge配置参考注释，感谢@asukanana,感谢@congcong.
 
-Surge显示机场的流量信息，url请求头中必须带有流量信息，并且需要urlencode
-
-[Proxy Group]
-SSinfo = select, policy-path=http://ss.info?url=xxx, update-interval=-1
+示例↓↓↓ 
+----------------------------------------
 
 [Script]
-Sub_Info = type=http-request,pattern=http://ss\.info,script-path=https://raw.githubusercontent.com/Wangsc1/All/master/Scripts/Sub_Info.js
+Sub_info = type=generic,timeout=10,script-path=https://raw.githubusercontent.com/mieqq/mieqq/master/sub_info_panel.js,script-update-interval=0,argument=url=[URL encode 后的机场节点链接]&reset_day=1&title=AmyInfo&icon=bonjour&color=#007aff
+
+[Panel]
+Sub_info = script-name=Sub_info,update-interval=600
+
+----------------------------------------
+
+先将带有流量信息的节点订阅链接encode，用encode后的链接替换"url="后面的[机场节点链接]
+
+可选参数 &reset_day，后面的数字替换成流量每月重置的日期，如1号就写1，8号就写8。如"&reset_day=8",不加该参数不显示流量重置信息。
+
+可选参数 &expire，机场链接不带expire信息的，可以手动传入expire参数，如"&expire=2022-02-01",注意一定要按照yyyy-MM-dd的格式。
+
+可选参数"title=xxx" 可以自定义标题。
+
+可选参数"icon=xxx" 可以自定义图标，内容为任意有效的 SF Symbol Name，如 bolt.horizontal.circle.fill，详细可以下载app https://apps.apple.com/cn/app/sf-symbols-browser/id1491161336
+
+可选参数"color=xxx" 当使用 icon 字段时，可传入 color 字段控制图标颜色，字段内容为颜色的 HEX 编码。如：color=#007aff
+----------------------------------------
 */
 
 (async () => {
-  let params = getUrlParams($request.url);
-  let info = await getUserInfo(params.url);
-  console.log('info:' + info)
-    if (!info) {
-    $notification.post("sub_info","","链接响应头不带有流量信息")
-    $done();
+  let params = getUrlParams($argument);
+  let resetDay = parseInt(params["reset_day"]);
+  let resetLeft = getRmainingDays(resetDay);
+  let usage = await getDataUsage(params.url);
+  let used = usage.download + usage.upload;
+  let total = usage.total;
+  let expire = usage.expire || params.expire;
+  let infoList = [`使用情况：${bytesToSize(used)} | ${bytesToSize(total)}`];
+
+  if (resetLeft) {
+    infoList.push(`重置日期：剩余${resetLeft}天`);
   }
-  let usage = getDataUsage(info);
-  let used = bytesToSize(usage.download + usage.upload);
-  let total = bytesToSize(usage.total);
-  let expire = usage.expire == undefined ? '' : ' ║ ' + formatTimestamp(usage.expire * 1000)
-  let http = "http, localhost, 6152";
-  let body = `${used} ⇋ ${total}${expire} = ${http}`;
-    $done({response: {body}});
+  if (expire) {
+    if (/^[\d]+$/.test(expire)) expire *= 1000;
+    infoList.push(`到期日期：${formatTime(expire)}`);
+  }
+  let now = new Date();
+  let hour = now.getHours();
+  let minutes = now.getMinutes();
+  hour = hour > 9 ? hour : "0" + hour;
+  minutes = minutes > 9 ? minutes : "0" + minutes;
+
+  let body = infoList.join("\n");
+  $done({
+    title: `${params.title} | ${hour}:${minutes}`,
+    content: body,
+    icon: params.icon || "airplane.circle",
+    "icon-color": params.color || "#007aff",
+  });
 })();
 
 function getUrlParams(url) {
-  return Object.fromEntries(url.slice(url.indexOf('?') + 1).split('&').map(item => item.split("=")).map(([k, v]) => [k, decodeURIComponent(v)]));   
+  return Object.fromEntries(
+    url
+      .split("&")
+      .map((item) => item.split("="))
+      .map(([k, v]) => [k, decodeURIComponent(v)])
+  );
 }
 
 function getUserInfo(url) {
-  let headers = {"User-Agent" :"Quantumult X"}
-  let request = {headers, url}
-  return new Promise(resolve => $httpClient.head(request, (err, resp) => 
-    resolve(resp.headers["subscription-userinfo"] || resp.headers["Subscription-userinfo"] || resp.headers["Subscription-Userinfo"])));
+  let request = { headers: { "User-Agent": "Quantumult%20X" }, url };
+  return new Promise((resolve) =>
+    $httpClient.head(request, (err, resp) => {
+      if (!resp) $done();
+      resolve(
+        resp.headers[
+          Object.keys(resp.headers).find(
+            (key) => key.toLowerCase() === "subscription-userinfo"
+          )
+        ]
+      );
+    })
+  );
 }
 
-function getDataUsage(info) {
-  return Object.fromEntries(info.match(/\w+=\d+/g).map(item => item.split("=")).map(([k, v]) => [k,parseInt(v)]));
+async function getDataUsage(url) {
+  let info = await getUserInfo(url);
+  if (!info) {
+    $notification.post("SubInfo", "", "链接响应头不带有流量信息");
+    $done();
+  }
+  return Object.fromEntries(
+    info
+      .match(/\w+=\d+/g)
+      .map((item) => item.split("="))
+      .map(([k, v]) => [k, parseInt(v)])
+  );
+}
+
+function getRmainingDays(resetDay) {
+  let now = new Date();
+  let today = now.getDate();
+  let month = now.getMonth();
+  let year = now.getFullYear();
+  if (!resetDay) return 0;
+  let daysInMonth = new Date(year, month + 1, 0).getDate();
+
+  if (resetDay > today) daysInMonth = 0;
+
+  return daysInMonth - today + resetDay;
 }
 
 function bytesToSize(bytes) {
-    if (bytes === 0) return '0B';
-    var k = 1024;
-    sizes = ['B','KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-    i = Math.floor(Math.log(bytes) / Math.log(k));
-    return (bytes / Math.pow(k, i)).toFixed(2) + sizes[i];
+  if (bytes === 0) return "0B";
+  let k = 1024;
+  sizes = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+  let i = Math.floor(Math.log(bytes) / Math.log(k));
+  return (bytes / Math.pow(k, i)).toFixed(2) + " " + sizes[i];
 }
 
-function formatTimestamp( timestamp ) {
-    var dateObj = new Date( timestamp );
-    var year = dateObj.getYear() + 1900;
-    var month = dateObj.getMonth() + 1;
-    month = month < 10 ? '0' + month : month
-    var day = dateObj.getDate();
-    day = day < 10 ? '0' + day : day
-    return year +"-"+ month +"-" + day;      
+function formatTime(time) {
+  let dateObj = new Date(time);
+  let year = dateObj.getFullYear();
+  let month = dateObj.getMonth() + 1;
+  let day = dateObj.getDate();
+  return year + "年" + month + "月" + day + "日";
 }
