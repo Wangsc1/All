df83caaa80002d490ecaff8539c4ce80b69d7297
Wangsc1 <Wangsc1M@gmail.com>
2021-09-28 19:05:01 +0800

    Sub_Info.js

--- Scripts/Sub_Info.js
+++ Scripts/Sub_Info.js
@@ -1,33 +1,23 @@
 /*
 Surge配置参考注释，感谢@asukanana,感谢@congcong.
-
 示例↓↓↓ 
 ----------------------------------------
-
 [Script]
 Sub_info = type=generic,timeout=10,script-path=https://raw.githubusercontent.com/mieqq/mieqq/master/sub_info_panel.js,script-update-interval=0,argument=url=[URL encode 后的机场节点链接]&reset_day=1&title=AmyInfo&icon=bonjour&color=#007aff
-
 [Panel]
-Sub_info = script-name=Sub_info,update-interval=600
-
+Sub_info = script-name=Sub_info
 ----------------------------------------
-
 先将带有流量信息的节点订阅链接encode，用encode后的链接替换"url="后面的[机场节点链接]
-
 可选参数 &reset_day，后面的数字替换成流量每月重置的日期，如1号就写1，8号就写8。如"&reset_day=8",不加该参数不显示流量重置信息。
-
 可选参数 &expire，机场链接不带expire信息的，可以手动传入expire参数，如"&expire=2022-02-01",注意一定要按照yyyy-MM-dd的格式。
-
 可选参数"title=xxx" 可以自定义标题。
-
 可选参数"icon=xxx" 可以自定义图标，内容为任意有效的 SF Symbol Name，如 bolt.horizontal.circle.fill，详细可以下载app https://apps.apple.com/cn/app/sf-symbols-browser/id1491161336
-
 可选参数"color=xxx" 当使用 icon 字段时，可传入 color 字段控制图标颜色，字段内容为颜色的 HEX 编码。如：color=#007aff
 ----------------------------------------
 */
 
 (async () => {
-  let params = getUrlParams($argument);
+  let params = getUrlParams($argument)
   let resetDay = parseInt(params["reset_day"]);
   let resetLeft = getRmainingDays(resetDay);
   let usage = await getDataUsage(params.url);
@@ -43,19 +33,14 @@ Sub_info = script-name=Sub_info,update-interval=600
     if (/^[\d]+$/.test(expire)) expire *= 1000;
     infoList.push(`到期：${formatTime(expire)}`);
   }
-  let now = new Date();
-  let hour = now.getHours();
-  let minutes = now.getMinutes();
-  hour = hour > 9 ? hour : "0" + hour;
-  minutes = minutes > 9 ? minutes : "0" + minutes;
 
   let body = infoList.join("\n");
   $done({
-    title: `${params.title} | ${hour}:${minutes}`,
-    content: body,
-    icon: params.icon || "airplane.circle",
-    "icon-color": params.color || "#007aff",
-  });
+		title: params.title,
+		content: body,
+               icon : params.icon || "airplane.circle",
+               "icon-color": params.color || "#007aff",
+	});
 })();
 
 function getUrlParams(url) {
@@ -70,16 +55,16 @@ function getUrlParams(url) {
 function getUserInfo(url) {
   let request = { headers: { "User-Agent": "Quantumult%20X" }, url };
   return new Promise((resolve) =>
-    $httpClient.head(request, (err, resp) => {
-      if (!resp) $done();
-      resolve(
-        resp.headers[
-          Object.keys(resp.headers).find(
-            (key) => key.toLowerCase() === "subscription-userinfo"
-          )
-        ]
-      );
-    })
+        $httpClient.head(request, (err, resp) => {
+          if (err) $done();
+          resolve(
+            resp.headers[
+              Object.keys(resp.headers).find(
+                (key) => key.toLowerCase() === "subscription-userinfo"
+              )
+            ]
+          );
+        }),
   );
 }
 
@@ -104,7 +89,7 @@ function getRmainingDays(resetDay) {
   let year = now.getFullYear();
   if (!resetDay) return 0;
   let daysInMonth = new Date(year, month + 1, 0).getDate();
-
+  
   if (resetDay > today) daysInMonth = 0;
 
   return daysInMonth - today + resetDay;
