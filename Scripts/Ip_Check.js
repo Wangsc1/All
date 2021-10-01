7946198839fe25c0d85232ed28bd37978e3cc925
Wangsc1 <Wangsc1M@gmail.com>
2021-10-01 08:43:17 +0800

    modified

--- Scripts/Ip_Check.js
+++ Scripts/Ip_Check.js
@@ -1,22 +1,37 @@
-$httpClient.get("http://ip-api.com/json", function(error, response, data){
-    let jsonData = JSON.parse(data)
-    let ip = jsonData.query
-    let country = jsonData.country
-    let emoji = getFlagEmoji(jsonData.countryCode)
-    let city = jsonData.city
-    let isp = jsonData.isp
-	$done({
-		title: `IP：    ${ip}`,
-		content: `ISP：  ${isp}\nLOC：${country} - ${city}`,
-        icon: "antenna.radiowaves.left.and.right.circle.fill",
-        "icon-color":"#56A4D8"
-	});
-});
+const { wifi, v4, v6 } = $network;
+
+// No network connection
+if (!v4.primaryAddress && !v6.primaryAddress) {
+    $done({
+      title: "网络信息",
+        content: "尚未连接网络\n请检查网络设置后重试",
+      icon: 'wifi.exclamationmark',
+      'icon-color': '#CB1B45',
+    });
+  }
+else{
+  $httpClient.get('http://ip-api.com/json', function (error, response, data) {
+    const jsonData = JSON.parse(data);
+    $done({
+      title: wifi.ssid ? wifi.ssid : '蜂窝网络',
+      content:
+        (v4.primaryAddress ? `IPv4 : ${v4.primaryAddress} \n` : '') +
+        (v6.primaryAddress ? `IPv6 : ${v6.primaryAddress}\n`: '') +
+        (v4.primaryRouter && wifi.ssid ? `Router IPv4 : ${v4.primaryRouter}\n` : '') +
+        (v6.primaryRouter && wifi.ssid ? `Router IPv6 : ${v6.primaryRouter}\n` : '') +
+        `节点IP : ${jsonData.query}\n` +
+        `节点ISP : ${jsonData.isp}\n` +
+        `节点LOC : ${getFlagEmoji(jsonData.countryCode)} | ${jsonData.country} - ${jsonData.city}`,
+      icon: wifi.ssid ? 'wifi' : 'simcard',
+      'icon-color': wifi.ssid ? '#005CAF' : '#F9BF45',
+    });
+  });
+};
 
 function getFlagEmoji(countryCode) {
-    const codePoints = countryCode
-      .toUpperCase()
-      .split('')
-      .map(char =>  127397 + char.charCodeAt());
-    return String.fromCodePoint(...codePoints);
+  const codePoints = countryCode
+    .toUpperCase()
+    .split('')
+    .map((char) => 127397 + char.charCodeAt());
+  return String.fromCodePoint(...codePoints);
 }
