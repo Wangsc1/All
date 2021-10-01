67467128d350668eb77420bf15b0e7626c356f92
Wangsc1 <Wangsc1M@gmail.com>
2021-10-01 08:50:49 +0800

    Ip_Check_1.js

--- Scripts/Ip_Check_1.js
+++ Scripts/Ip_Check_1.js
@@ -1,50 +1,37 @@
- const { wifi, v4 } = $network;
- const v4IP = v4.primaryAddress;
- let url = "http://ip-api.com/json"
+const { wifi, v4, v6 } = $network;
 
- ;(async () => {
-    let result ={
-        title: "网络信息",
+// No network connection
+if (!v4.primaryAddress && !v6.primaryAddress) {
+    $done({
+      title: "网络信息",
         content: "尚未连接网络\n请检查网络设置后重试",
-        icon: "wifi.exclamationmark",
-        'icon-color': "#CB1B45"
-    }
-    // No network connection
-    if (!v4IP) {
-        result['title'] = "网络信息"
-        result['content'] = "尚未连接网络\n请检查网络设置后重试"
-        result['icon'] = "wifi.exclamationmark"
-        result['icon-color'] = "#CB1B45"
-        $done(result)
-        return
-    }
-    const ip = v4IP;
-    const router = wifi.ssid ? v4.primaryRouter : undefined;
-    
-    $httpClient.get(url, function(error, response, data){
-        let jsonData = JSON.parse(data)
-        let externalIP = jsonData.query
-        let country = jsonData.country
-        let emoji = getFlagEmoji(jsonData.countryCode)
-        let city = jsonData.city
-        let isp = jsonData.isp
-        result['title'] =  wifi.ssid ? wifi.ssid : "蜂窝网络  |  中国联通"
-        result['content'] = (wifi.ssid ? `路由 IP：  ${router}\n` : "")
-                            + (wifi.ssid ? `本机 IP：  ${ip} \n` : `本机 IP：  ${ip} \n`)
-                            + (wifi.ssid ? `节点 IP：  ${externalIP}\n` : `节点 IP：  ${externalIP}\n`)
-                            + (wifi.ssid ? `节点 ISP :  ${isp}\n` : `节点 ISP :  ${isp}\n`)
-                            + (wifi.ssid ? `节点LOC : ${country} - ${city}` : `节点LOC : ${country} - ${city}`)
-        result['icon'] = wifi.ssid ? "wifi.circle.fill" : "antenna.radiowaves.left.and.right.circle.fill"
-        result['icon-color'] = wifi.ssid ? "#3478F6" : "#65C466"
-        $done(result)
-        return
-    })
- })()
- 
- function getFlagEmoji(countryCode) {
-    const codePoints = countryCode
-      .toUpperCase()
-      .split('')
-      .map(char =>  127397 + char.charCodeAt());
-    return String.fromCodePoint(...codePoints);
+      icon: 'wifi.exclamationmark',
+      'icon-color': '#CB1B45',
+    });
+  }
+else{
+  $httpClient.get('http://ip-api.com/json', function (error, response, data) {
+    const jsonData = JSON.parse(data);
+    $done({
+      title: wifi.ssid ? wifi.ssid : '蜂窝网络  |  中国联通',
+      content:
+        (v4.primaryAddress ? `IPv4 : ${v4.primaryAddress} \n` : '') +
+        (v6.primaryAddress ? `IPv6 : ${v6.primaryAddress}\n`: '') +
+        (v4.primaryRouter && wifi.ssid ? `Router IPv4 : ${v4.primaryRouter}\n` : '') +
+        (v6.primaryRouter && wifi.ssid ? `Router IPv6 : ${v6.primaryRouter}\n` : '') +
+        `节点IP : ${jsonData.query}\n` +
+        `节点ISP : ${jsonData.isp}\n` +
+        `节点LOC : ${jsonData.country} - ${jsonData.city}`,
+      icon: wifi.ssid ? 'wifi.circle.fill' : 'antenna.radiowaves.left.and.right.circle.fill',
+      'icon-color': wifi.ssid ? '#3478F6' : '#65C466',
+    });
+  });
+};
+
+function getFlagEmoji(countryCode) {
+  const codePoints = countryCode
+    .toUpperCase()
+    .split('')
+    .map((char) => 127397 + char.charCodeAt());
+  return String.fromCodePoint(...codePoints);
 }
