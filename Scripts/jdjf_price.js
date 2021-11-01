17229eab88711627121b212c0da6f3f33a66088e
Wangsc1 <Wangsc1M@gmail.com>
2021-11-01 16:09:09 +0800

    jdjf_price.js

--- Scripts/jdjf_price.js
+++ Scripts/jdjf_price.js
@@ -355,7 +355,7 @@ function convert(url, isOriginJXURL) {
                         let r = {};
                         let scheme = autoChoose ? autoScheme : chooseScheme;
                         if (data.data.promotionUrl) {
-                            r.msg = `〽️ 佣金比例 ➩ ${data.data.wlCommissionShare}%║💴 预估佣金 ➩ ${data.data.wlCommission}¥`;
+                            r.msg = `💴 预估佣金 ➩ ${data.data.wlCommission}¥〽️║佣金比例 ➩ ${data.data.wlCommissionShare}%`;
                             r.convertURL =
                                 scheme == "browser"
                                     ? chooseBrowser + data.data.promotionUrl
