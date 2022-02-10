463c3a89e16174f71991b4a6ea3838e7e4e71b51
Wangsc1 <Wangsc1M@gmail.com>
2022-02-10 12:14:59 +0800

    Sub_Info.js

--- Scripts/Sub_Info.js
+++ Scripts/Sub_Info.js
@@ -1,5 +1,6 @@
+let args = getArgs();
+
 (async () => {
-  let args = getArgs();
   let info = await getDataInfo(args.url);
   if (!info) $done();
   let resetDayLeft = getRmainingDays(parseInt(args["reset_day"]));
@@ -42,9 +43,10 @@ function getArgs() {
 }
 
 function getUserInfo(url) {
+  let method = args.method || "head";
   let request = { headers: { "User-Agent": "Quantumult%20X" }, url };
   return new Promise((resolve, reject) =>
-    $httpClient.get(request, (err, resp) => {
+    $httpClient[method](request, (err, resp) => {
       if (err != null) {
         reject(err);
         return;
