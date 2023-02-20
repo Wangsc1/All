b4936bffb202349b5baa3b6d4822e14ebfc1c6cf
Wangsc1 <Wangsc1M@gmail.com>
2023-02-20 13:43:09 +0800

    Ping.js

--- Scripts/Ping.js
+++ Scripts/Ping.js
@@ -2,12 +2,11 @@
 
 let $ = {
 Baidu:'https://www.baidu.com',
-Google:'https://www.google.com/generate_204',
-Github:'https://www.github.com'
+Google:'https://www.google.com/generate_204'
 }
 
 !(async () => {
-await Promise.all([http('Baidu'),http('Github'),http('Google')]).then((x)=>{
+await Promise.all([http('Baidu'),http('Google')]).then((x)=>{
 	$done({
     title: 'Ping Test',
     content: x.join(' | '),
@@ -22,8 +21,8 @@ function http(req) {
 			let time = Date.now();
         $httpClient.post($[req], (err, resp, data) => {
             r(req +
-						'\xa0\xa0\xa0\t:' +
-						(Date.now() - time)+' ms');
+						': ' +
+						(Date.now() - time)+'ms');
         });
     });
 }
